f3fb25007b6f4f70667cc3ab00483a049b23c728
silacode <siladitya005@gmail.com>
2021-07-24 11:59:23 -0400

    passport google strategy and passport local srategy for authentication

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //------------------------------------------- Siladitya Samaddar -------------------------------------------------------//
 //
-// passport local strategy for cookie based session authorization
+// passport google and local strategy for cookie based session authorization
 // authenticated routes
-// passport, passport-local, passport-local-mongoose, express-session modules are used.
+//
 //
 //-----------------------------------------------------------------------------------------------------------------------//
 
@@ -15,10 +15,13 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 //also install passport-local
 
 //........................... constants ..............................
 const secret = process.env.SECRET;
+const PORT = 4000;
 
 //........................... app setup ..............................
 const app = express();
@@ -62,31 +65,88 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: String,
   password: String,
+  googleId: String,
+  secret: String,
 });
 //passport-local-mongoose is used to hash and salt password and save to database
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 //.............................. passport setup .............................
 //passport local login strategy
 passport.use(User.createStrategy());
 //serialize and deserialize user into cookie
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) =>
+  User.findById(id, (err, user) => done(err, user))
+);
+//google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    (accessToken, refreshToken, profile, cb) =>
+      //google login register both logic
+      User.findOrCreate({ googleId: profile.id }, (err, user) => {
+        return cb(err, user);
+      })
+  )
+);
 
 //.............................. routes ..................................
 app.get("/", (req, res) => res.render("home"));
+
+//google authentication route
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  // Successful authentication, redirect secrets.
+  (req, res) => res.redirect("/secrets")
+);
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
-//authenticated route
+
 app.get("/secrets", (req, res) => {
-  req.isAuthenticated() ? res.render("secrets") : res.redirect("/login");
+  User.find({ secret: { $ne: null } }, (err, foundUsers) => {
+    if (err) console.log(err);
+    else if (foundUsers)
+      res.render("secrets", { usersWithSecrets: foundUsers });
+  });
 });
+
+//authenticated get submit route
+// req.isAuthenticated() method is used to check if logged in or not
+app.get("/submit", (req, res) => {
+  req.isAuthenticated() ? res.render("submit") : res.redirect("/login");
+});
+
+app.post("/submit", (req, res) => {
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err) console.log(err);
+    else if (foundUser) {
+      foundUser.secret = submittedSecret;
+      foundUser.save(() => res.redirect("/secrets"));
+    }
+  });
+});
+
+//logout route
 app.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
-//register route
+
+//register route local strategy
 app.post("/register", (req, res) => {
   const { username, password } = req.body;
   //passport method for register
@@ -98,7 +158,8 @@ app.post("/register", (req, res) => {
     passport.authenticate("local")(req, res, () => res.redirect("/secrets"));
   });
 });
-//login route
+
+//login route local strategy
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   const user = new User({ username, password });
@@ -110,6 +171,6 @@ app.post("/login", (req, res) => {
 });
 
 //................................ listen ...................................s
-app.listen(3000, () => {
-  console.log("Server is ruuning at port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is ruuning at port ${PORT}`);
 });
